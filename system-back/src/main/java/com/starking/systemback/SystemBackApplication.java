bc775841b89dd8da01beffe1977f7ccaef61418e
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-04-23 20:38:00 -0300

    Criando o back

---
+++ system-back/src/main/java/com/starking/systemback/SystemBackApplication.java
@@ -0,0 +1,13 @@
+package com.starking.systemback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemBackApplication.class, args);
+	}
+
+}
---
+++ system-back/src/test/java/com/starking/systemback/SystemBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starking.systemback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
