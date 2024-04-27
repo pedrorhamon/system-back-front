76c1a33fd6bb0df98fecc4567d543199ab2cb643
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-04-27 16:08:05 -0300

    to quase desistindo

--- money-front/src/app/app-routing.module.ts
+++ money-front/src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
-import { HomeComponent } from './home/home.component';
+import { NavComponent } from './template/nav/nav.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'novo', component: NovoUsuarioComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'nav', component: NavComponent },
 ];
 
 @NgModule({
