87815e7405b9800135ba21de17b58a5e5b27c587
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-04-27 15:56:19 -0300

    Mola do caramba pra colocar isso

--- money-front/src/app/app-routing.module.ts
+++ money-front/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'novo', component: NovoUsuarioComponent }
+  { path: 'novo', component: NovoUsuarioComponent },
+  { path: 'home', component: HomeComponent },
 ];
 
 @NgModule({
--- money-front/src/app/app.module.ts
+++ money-front/src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { PasswordModule } from 'primeng/password';
+import { SidebarModule } from 'primeng/sidebar';
 import { TableModule } from 'primeng/table';
+import { ToolbarModule } from 'primeng/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
+import { FooterComponent } from './template/footer/footer.component';
+import { HeaderComponent } from './template/header/header.component';
+import { NavComponent } from './template/nav/nav.component';
 
 
 
@@ -18,7 +24,10 @@ import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
     AppComponent,
     LoginComponent,
     HomeComponent,
-    NovoUsuarioComponent
+    NovoUsuarioComponent,
+    NavComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -26,8 +35,10 @@ import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
     TableModule,
     InputTextModule,
     PasswordModule,
-    ButtonModule
-
+    ButtonModule,
+    SidebarModule,
+    BrowserAnimationsModule,
+    ToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
