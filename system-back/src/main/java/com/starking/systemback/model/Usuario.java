c154bd96f48ac5c44768c22f91a7bae0828b697e
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-05-01 16:42:40 -0300

    iniciando entidade de Usuario

---
+++ system-back/src/main/java/com/starking/systemback/model/Usuario.java
@@ -0,0 +1,12 @@
+package com.starking.systemback.model;
+
+import java.io.Serializable;
+
+/**
+ * @author pedroRhamon
+ */
+public class Usuario implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+}
