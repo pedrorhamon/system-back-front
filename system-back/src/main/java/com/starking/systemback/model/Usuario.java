02e40a1be2411aff1fcc3014bf963931056391b6
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-05-01 16:44:48 -0300

    fazendo mapeamento inicial

--- system-back/src/main/java/com/starking/systemback/model/Usuario.java
+++ system-back/src/main/java/com/starking/systemback/model/Usuario.java
@@ -2,11 +2,24 @@ package com.starking.systemback.model;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 /**
  * @author pedroRhamon
  */
+
+@Entity
+@Table(name = "usuario")
 public class Usuario implements Serializable{
 
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
 }
