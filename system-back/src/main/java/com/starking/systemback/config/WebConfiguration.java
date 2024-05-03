e38eeefb94c5a9fa6134145ac92c4db3e6a2e82b
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-05-03 20:36:48 -0300

    Tratando cors

--- system-back/src/main/java/com/starking/systemback/config/WebConfiguration.java
+++ system-back/src/main/java/com/starking/systemback/config/WebConfiguration.java
@@ -1,8 +1,20 @@
 package com.starking.systemback.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 /**
  * @author pedroRhamon
  */
-public class WebConfiguration {
 
+@EnableWebMvc
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+	}	
 }
