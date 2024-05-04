751a1e65b6e36066528e5f2982ecd4722b348c8b
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-05-04 19:06:01 -0300

    instanciando o repository

--- system-back/src/main/java/com/starking/systemback/services/UsuarioService.java
+++ system-back/src/main/java/com/starking/systemback/services/UsuarioService.java
@@ -1,8 +1,17 @@
 package com.starking.systemback.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.starking.systemback.repositories.UsuarioRepository;
+
 /**
  * @author pedroRhamon
  */
+@Service
 public class UsuarioService {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 
 }
