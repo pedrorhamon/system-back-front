2f7f155a63c1a175f7001471c459f5b97465bd17
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-05-04 20:05:47 -0300

    Criando Exception

---
+++ system-back/src/main/java/com/starking/systemback/RegraNegocioException.java
@@ -0,0 +1,13 @@
+package com.starking.systemback;
+
+/**
+ * @author pedroRhamon
+ */
+public class RegraNegocioException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public RegraNegocioException(String message) {
+		super(message);
+	}
+}
