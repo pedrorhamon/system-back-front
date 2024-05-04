545572a04ab429fcd11778999d0d15f9fe8010db
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-05-04 19:49:53 -0300

    criando metodo de obterLoginUsuario e o claims

--- system-back/src/main/java/com/starking/systemback/services/JwtService.java
+++ system-back/src/main/java/com/starking/systemback/services/JwtService.java
@@ -1,8 +1,32 @@
 package com.starking.systemback.services;
 
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+
 /**
  * @author pedroRhamon
  */
+
+@Service
 public class JwtService {
+	
+	private String chaveAssinatura; 
+	
+	
+	public String obterLoginUsuario(String token) {
+		Claims claims = obterClaims(token);
+		return claims.getSubject();
+	}
+	
+	private Claims obterClaims(String token) throws ExpiredJwtException {
+		return Jwts
+				.parser()
+				.setSigningKey(chaveAssinatura)
+				.parseClaimsJws(token)
+				.getBody();
+	}
 
 }
