3caf573dcfb051ba035c2565c64de23a7710ca4a
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-05-04 20:20:30 -0300

    metodo de criptografar senha

--- system-back/src/main/java/com/starking/systemback/services/UsuarioService.java
+++ system-back/src/main/java/com/starking/systemback/services/UsuarioService.java
@@ -3,8 +3,10 @@ package com.starking.systemback.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.starking.systemback.model.Usuario;
 import com.starking.systemback.model.response.UsuarioResponse;
 import com.starking.systemback.repositories.UsuarioRepository;
 
@@ -17,7 +19,16 @@ public class UsuarioService {
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 	
+	@Autowired
+	private PasswordEncoder encoder;
+	
 	public Page<UsuarioResponse> findAll(Pageable pageable) {
 		return usuarioRepository.findAll(pageable).map(UsuarioResponse::new);
+	}
+	
+	private void criptografarSenha(Usuario usuario) {
+		String senha = usuario.getSenha();
+		String senhaCripto = encoder.encode(senha);
+		usuario.setSenha(senhaCripto);
 	}
 }
