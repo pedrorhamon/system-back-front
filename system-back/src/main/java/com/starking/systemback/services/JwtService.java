6794cfbfeb375d7473ee47aa85b2de87da3ce24c
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-05-04 19:52:08 -0300

    isTokenValido

--- system-back/src/main/java/com/starking/systemback/services/JwtService.java
+++ system-back/src/main/java/com/starking/systemback/services/JwtService.java
@@ -1,5 +1,8 @@
 package com.starking.systemback.services;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Claims;
@@ -28,5 +31,12 @@ public class JwtService {
 				.parseClaimsJws(token)
 				.getBody();
 	}
-
+	
+	public boolean isTokenValido(String token) throws ExpiredJwtException {
+		Claims claims = obterClaims(token);
+		java.util.Date dataEx = claims.getExpiration();
+		LocalDateTime dataExpiracao = dataEx.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+		boolean dataHoraAtualIsAfterDataExpiracao = LocalDateTime.now().isAfter(dataExpiracao);
+		return !dataHoraAtualIsAfterDataExpiracao;
+	}
 }
