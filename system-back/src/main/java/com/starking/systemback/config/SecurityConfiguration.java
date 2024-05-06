cf7c27f0c2859fc75cac514bd65842ca6ce6f670
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-05-06 19:58:34 -0300

    adicionando anotacoes e injentando

--- system-back/src/main/java/com/starking/systemback/config/SecurityConfiguration.java
+++ system-back/src/main/java/com/starking/systemback/config/SecurityConfiguration.java
@@ -1,10 +1,23 @@
 package com.starking.systemback.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import com.starking.systemback.services.JwtService;
+import com.starking.systemback.services.SecurityUserDetailsService;
 
 /**
  * @author pedroRhamon
  */
+
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private SecurityUserDetailsService userDetailsService;
+	
+	@Autowired
+	private JwtService jwtService;
 
 }
