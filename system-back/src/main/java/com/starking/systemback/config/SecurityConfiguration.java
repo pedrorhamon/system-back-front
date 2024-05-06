801414f92b0fac65f671478b7af1ad7f3fca85d0
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-05-06 19:57:47 -0300

    Criando a class SecurityConfiguration

---
+++ system-back/src/main/java/com/starking/systemback/config/SecurityConfiguration.java
@@ -0,0 +1,10 @@
+package com.starking.systemback.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author pedroRhamon
+ */
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+}
