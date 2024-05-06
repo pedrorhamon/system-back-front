5826ba28d8f7052d49c430383e67936ad0c78735
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-05-06 20:02:34 -0300

    Adicionando metodo doFilterInternal

--- system-back/src/main/java/com/starking/systemback/config/JwtTokenFilter.java
+++ system-back/src/main/java/com/starking/systemback/config/JwtTokenFilter.java
@@ -8,6 +8,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.starking.systemback.services.JwtService;
@@ -27,8 +31,30 @@ public class JwtTokenFilter extends OncePerRequestFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		
+		String authorization = request.getHeader("Authorization");
+
+		// "Bearer","eyJhbGciOiJIUzUxMiJ9.eyJ..."
+
+		if (authorization != null && authorization.startsWith("Bearer")) {
+
+			String token = authorization.split(" ")[1];
+			boolean isTokenValid = jwtService.isTokenValido(token);
+
+			if (isTokenValid) {
+				String login = jwtService.obterLoginUsuario(token);
+				UserDetails usuarioAutenticado = userDetailsService.loadUserByUsername(login);
+
+				UsernamePasswordAuthenticationToken user = new UsernamePasswordAuthenticationToken(usuarioAutenticado,
+						null, usuarioAutenticado.getAuthorities());
+
+				user.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+				SecurityContextHolder.getContext().setAuthentication(user);
+
+			}
+		}
+
+		filterChain.doFilter(request, response);
 	}
 
 }
