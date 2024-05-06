70227557d6def77775b7b6e175b73ad0409a38c1
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-05-06 20:01:37 -0300

    injentando e implementando

--- system-back/src/main/java/com/starking/systemback/config/JwtTokenFilter.java
+++ system-back/src/main/java/com/starking/systemback/config/JwtTokenFilter.java
@@ -1,8 +1,34 @@
 package com.starking.systemback.config;
 
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.starking.systemback.services.JwtService;
+import com.starking.systemback.services.SecurityUserDetailsService;
+
 /**
  * @author pedroRhamon
  */
-public class JwtTokenFilter {
+public class JwtTokenFilter extends OncePerRequestFilter {
+	
+	@Autowired
+	private JwtService jwtService;
+	
+	@Autowired
+	private SecurityUserDetailsService userDetailsService;
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
