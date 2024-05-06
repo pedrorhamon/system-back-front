e4eae265bd8ccff5f05f3bc70c4b1143e10c20a2
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-05-06 19:59:08 -0300

    criando o PasswordEncoder

--- system-back/src/main/java/com/starking/systemback/config/SecurityConfiguration.java
+++ system-back/src/main/java/com/starking/systemback/config/SecurityConfiguration.java
@@ -1,8 +1,11 @@
 package com.starking.systemback.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.starking.systemback.services.JwtService;
 import com.starking.systemback.services.SecurityUserDetailsService;
@@ -19,5 +22,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	private JwtService jwtService;
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		PasswordEncoder encoder = new BCryptPasswordEncoder();
+		return encoder;
+	}
 
 }
