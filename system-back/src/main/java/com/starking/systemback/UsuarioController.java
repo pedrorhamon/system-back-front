e9fd5895b838a0089f7e0388c99e5408f659ffe9
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-05-07 19:09:33 -0300

    Injetando o service

--- system-back/src/main/java/com/starking/systemback/UsuarioController.java
+++ system-back/src/main/java/com/starking/systemback/UsuarioController.java
@@ -1,7 +1,11 @@
 package com.starking.systemback;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.starking.systemback.services.JwtService;
+import com.starking.systemback.services.UsuarioService;
 
 /**
  * @author pedroRhamon
@@ -10,5 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/usuario")
 public class UsuarioController {
+	
+	@Autowired
+	private UsuarioService usuarioService;
+	
+	@Autowired
+	private JwtService jwtService;
 
 }
