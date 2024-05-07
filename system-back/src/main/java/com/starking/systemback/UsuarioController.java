c38e0bc850f40d27027cc6efca7ba29faa667a94
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-05-07 19:13:12 -0300

    criando metodo de obterPorId

--- system-back/src/main/java/com/starking/systemback/UsuarioController.java
+++ system-back/src/main/java/com/starking/systemback/UsuarioController.java
@@ -1,6 +1,9 @@
 package com.starking.systemback;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +23,11 @@ public class UsuarioController {
 	
 	@Autowired
 	private JwtService jwtService;
+	
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?>obterPorId( @PathVariable("id") Long id ) {
+		return ResponseEntity.ok().body(this.usuarioService.obterPorId(id).get());
+	}
 
 }
