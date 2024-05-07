1b832cfc20b3dfb8d96d189c411efb59dd292b51
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-05-07 19:04:40 -0300

    Criando interface de Usuario no front

--- money-front/src/app/usuario/usuario.service.ts
+++ money-front/src/app/usuario/usuario.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Usuario } from '../../model/Usuario';
 
 @Injectable({
   providedIn: 'root'
---
+++ money-front/src/model/usuario.ts
@@ -0,0 +1,8 @@
+export interface Usuario {
+  id: number;
+  name: string;
+  email: string;
+  cpf: string;
+  isAtivo: boolean;
+  perfil: string;
+}
