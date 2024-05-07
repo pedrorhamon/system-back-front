08ca48d982b36f2cc3003f8edaeff430820140f1
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-05-07 19:39:30 -0300

    buscando todos

--- system-back/src/main/java/com/starking/systemback/UsuarioController.java
+++ system-back/src/main/java/com/starking/systemback/UsuarioController.java
@@ -1,12 +1,15 @@
 package com.starking.systemback;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.starking.systemback.model.response.UsuarioResponse;
 import com.starking.systemback.services.JwtService;
 import com.starking.systemback.services.UsuarioService;
 
@@ -24,6 +27,10 @@ public class UsuarioController {
 	@Autowired
 	private JwtService jwtService;
 	
+	@GetMapping
+	public ResponseEntity<Page<UsuarioResponse>> buscarTodos(Pageable pageable) {
+		return ResponseEntity.ok().body(this.usuarioService.findAll(pageable));
+	}
 	
 	@GetMapping("/{id}")
 	public ResponseEntity<?>obterPorId( @PathVariable("id") Long id ) {
