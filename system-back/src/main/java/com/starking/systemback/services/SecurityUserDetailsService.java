f461678580bd2b2694a2be423a45178f0e667644
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-05-07 19:19:03 -0300

    adicionando o @service em uma class principal

--- system-back/src/main/java/com/starking/systemback/services/SecurityUserDetailsService.java
+++ system-back/src/main/java/com/starking/systemback/services/SecurityUserDetailsService.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.starking.systemback.model.Usuario;
 import com.starking.systemback.repositories.UsuarioRepository;
@@ -12,6 +13,7 @@ import com.starking.systemback.repositories.UsuarioRepository;
 /**
  * @author pedroRhamon
  */
+@Service
 public class SecurityUserDetailsService implements UserDetailsService{
 	
 	@Autowired
