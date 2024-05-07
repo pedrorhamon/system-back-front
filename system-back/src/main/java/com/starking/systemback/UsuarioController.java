a9045a1bb33c3be4fdd5a1a80f1428cdaeef057b
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-05-07 19:06:23 -0300

    Utilizando anotacoes

--- system-back/src/main/java/com/starking/systemback/UsuarioController.java
+++ system-back/src/main/java/com/starking/systemback/UsuarioController.java
@@ -1,8 +1,14 @@
 package com.starking.systemback;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author pedroRhamon
  */
+
+@RestController
+@RequestMapping("/api/usuario")
 public class UsuarioController {
 
 }
