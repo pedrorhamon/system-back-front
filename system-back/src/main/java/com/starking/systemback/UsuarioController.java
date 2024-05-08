3dd7f17ac37933420c1163b748635205754ff3f9
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-05-08 18:38:14 -0300

    endpoint de atualizar

--- system-back/src/main/java/com/starking/systemback/UsuarioController.java
+++ system-back/src/main/java/com/starking/systemback/UsuarioController.java
@@ -10,8 +10,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.starking.systemback.model.Usuario;
@@ -28,28 +30,33 @@ import com.starking.systemback.services.UsuarioService;
 @RestController
 @RequestMapping("/api/usuario")
 public class UsuarioController {
-	
+
 	@Autowired
 	private UsuarioService usuarioService;
-	
+
 	@Autowired
 	private JwtService jwtService;
-	
+
 	@PostMapping
 	public ResponseEntity<?> salvarUsuario(@RequestBody @Valid Usuario usuario) throws RegraNegocioException {
-			return ResponseEntity.status(HttpStatus.CREATED).body(this.usuarioService.salvarUsuario(usuario));
+		return ResponseEntity.status(HttpStatus.CREATED).body(this.usuarioService.salvarUsuario(usuario));
 	}
-	
+
+	@PutMapping("/{id}")
+	public ResponseEntity<?> atualizarUsuario(@RequestParam("id") Long id, @RequestBody Usuario usuarioAtualizado) throws RegraNegocioException {
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).body(this.usuarioService.atualizarUsuario(id, usuarioAtualizado));
+	}
+
 	@GetMapping
 	public ResponseEntity<Page<UsuarioResponse>> buscarTodos(Pageable pageable) {
 		return ResponseEntity.ok().body(this.usuarioService.findAll(pageable));
 	}
-	
+
 	@GetMapping("/{id}")
-	public ResponseEntity<?>obterPorId( @PathVariable("id") Long id ) {
+	public ResponseEntity<?> obterPorId(@PathVariable("id") Long id) {
 		return ResponseEntity.ok().body(this.usuarioService.obterPorId(id).get());
 	}
-	
+
 	@PostMapping("/autenticar")
 	public ResponseEntity<?> autenticar(@RequestBody UsuarioRequest usuarioRequest) throws ErroAutenticacao {
 		return ResponseEntity.ok(usuarioService.autenticar(usuarioRequest.getEmail(), usuarioRequest.getSenha()));
