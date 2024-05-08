ccccb62a25b30fe15be704dd16f505956c2e9eac
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-05-08 17:29:08 -0300

    Criando endpoint para salvar usuario

--- system-back/src/main/java/com/starking/systemback/UsuarioController.java
+++ system-back/src/main/java/com/starking/systemback/UsuarioController.java
@@ -1,14 +1,20 @@
 package com.starking.systemback;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.starking.systemback.model.Usuario;
 import com.starking.systemback.model.response.UsuarioResponse;
 import com.starking.systemback.services.JwtService;
 import com.starking.systemback.services.UsuarioService;
@@ -26,6 +32,11 @@ public class UsuarioController {
 	
 	@Autowired
 	private JwtService jwtService;
+	
+	@PostMapping
+	public ResponseEntity<UsuarioResponse> salvarUsuario(@RequestBody @Valid Usuario usuario) throws RegraNegocioException {
+			return ResponseEntity.status(HttpStatus.CREATED).body(this.usuarioService.salvarUsuario(usuario));
+	}
 	
 	@GetMapping
 	public ResponseEntity<Page<UsuarioResponse>> buscarTodos(Pageable pageable) {
