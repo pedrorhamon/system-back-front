4faa194872ae41101db27f41836d384d40d66651
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-05-09 17:32:55 -0300

    criando service de salvar usuario

--- money-front/src/app/usuario/usuario.service.ts
+++ money-front/src/app/usuario/usuario.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Usuario } from '../../model/Usuario';
+import { catchError } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +13,20 @@ export class UsuarioService {
 
   constructor(private httpClient: HttpClient) { }
 
+  salvarUsuario(usuario: Usuario): Observable<any> {
+    return this.httpClient.post<Usuario>(this.baseUrl, usuario)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
   findAll() {
     return this.httpClient.get<Usuario>(`${this.baseUrl}`);
+  }
+
+
+  private handleError(error: any): Observable<any> {
+    console.error('Ocorreu um erro:', error);
+    return throwError(error);
   }
 }
