9e47a2b1a9dd2c912d8acdb055d106c1e5fed95a
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-05-10 17:24:04 -0300

    criando function de buscar todos usuarios

--- money-front/src/app/login/login.component.ts
+++ money-front/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UsuarioService } from '../usuario/usuario.service';
+import { Usuario } from '../../model/Usuario';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  constructor(private usuarioService: UsuarioService) { }
+
+  buscarTodos() {
+    this.usuarioService.findAll().subscribe(
+    (usuarios: Usuario) => {
+      console.log('Usuarios: ', usuarios);
+    },
+    error => {
+      console.error('Erro ao buscar usuários:', error);
+    }
+  )
+  }
 
 }
