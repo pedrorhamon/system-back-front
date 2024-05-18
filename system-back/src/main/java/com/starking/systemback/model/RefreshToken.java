c8b15b3f454e909fc78ef6f717ede0c2db476682
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-05-17 21:09:09 -0300

    Criando atributos

--- system-back/src/main/java/com/starking/systemback/model/RefreshToken.java
+++ system-back/src/main/java/com/starking/systemback/model/RefreshToken.java
@@ -1,8 +1,28 @@
 package com.starking.systemback.model;
 
+import java.time.Instant;
+
+import javax.persistence.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @author pedroRhamon
  */
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class RefreshToken {
 
+	private String token;
+	
+	private Instant expiryDate;
+	
+	private Usuario usuario;
 }
