e87ac58daba643d449d1623416ba69468d59d2d4
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-05-18 17:36:01 -0300

    Refresh token service injecao

--- system-back/src/main/java/com/starking/systemback/services/RefreshTokenService.java
+++ system-back/src/main/java/com/starking/systemback/services/RefreshTokenService.java
@@ -1,8 +1,18 @@
 package com.starking.systemback.services;
 
+import org.springframework.stereotype.Service;
+
+import com.starking.systemback.repositories.RefreshTokenRepository;
+
+import lombok.AllArgsConstructor;
+
 /**
  * @author pedroRhamon
  */
+
+@Service
+@AllArgsConstructor
 public class RefreshTokenService {
 
+	private final RefreshTokenRepository refreshTokenRepository;
 }
