c5ae91da5dda631fdda59fb11add2022e04eb7e4
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-05-18 17:41:55 -0300

    criando o metodo createRefreshToken

--- system-back/src/main/java/com/starking/systemback/services/RefreshTokenService.java
+++ system-back/src/main/java/com/starking/systemback/services/RefreshTokenService.java
@@ -1,8 +1,13 @@
 package com.starking.systemback.services;
 
+import java.time.Instant;
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 
+import com.starking.systemback.model.RefreshToken;
 import com.starking.systemback.repositories.RefreshTokenRepository;
+import com.starking.systemback.repositories.UsuarioRepository;
 
 import lombok.AllArgsConstructor;
 
@@ -15,4 +20,15 @@ import lombok.AllArgsConstructor;
 public class RefreshTokenService {
 
 	private final RefreshTokenRepository refreshTokenRepository;
+	
+	private final UsuarioRepository usuarioRepository;
+	
+	public RefreshToken createRefreshToken(String username) {
+		RefreshToken refreshToken = RefreshToken.builder()
+				.usuario(this.usuarioRepository.findByEmail(username).get())
+				.token(UUID.randomUUID().toString())
+				.expiryDate(Instant.now().plusMillis(600000)) // set expiry of refresh token to 10 minutes - you can configure it application.properties file 
+				.build();
+		return this.refreshTokenRepository.save(refreshToken);
+	}
 }
