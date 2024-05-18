cc21148065ce57ca4c404b67db505df9ad8d18eb
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-05-17 21:56:41 -0300

    criando findByToken

--- system-back/src/main/java/com/starking/systemback/repositories/RefreshTokenRepository.java
+++ system-back/src/main/java/com/starking/systemback/repositories/RefreshTokenRepository.java
@@ -1,5 +1,7 @@
 package com.starking.systemback.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.starking.systemback.model.RefreshToken;
@@ -9,4 +11,5 @@ import com.starking.systemback.model.RefreshToken;
  */
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long>{
 
+	Optional<RefreshToken> findByToken(String token);
 }
