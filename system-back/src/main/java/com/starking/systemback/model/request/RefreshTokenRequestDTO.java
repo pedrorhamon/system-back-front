9ed147fa453319ba56ac54c5960e382011caf51a
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-05-20 17:33:42 -0300

    adicionando atributo

--- system-back/src/main/java/com/starking/systemback/model/request/RefreshTokenRequestDTO.java
+++ system-back/src/main/java/com/starking/systemback/model/request/RefreshTokenRequestDTO.java
@@ -1,8 +1,17 @@
 package com.starking.systemback.model.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @author pedroRhamon
  */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RefreshTokenRequestDTO {
 
+	private String token;
 }
