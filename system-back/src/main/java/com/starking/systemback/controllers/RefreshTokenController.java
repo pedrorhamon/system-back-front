a375f6471d3c0a25d60940a1ebeeb047eda83184
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-05-21 20:20:34 -0300

    criando endpoint para refresh token

---
+++ system-back/src/main/java/com/starking/systemback/controllers/RefreshTokenController.java
@@ -0,0 +1,14 @@
+package com.starking.systemback.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author pedroRhamon
+ */
+
+@RestController
+@RequestMapping("/refresh")
+public class RefreshTokenController {
+
+}
