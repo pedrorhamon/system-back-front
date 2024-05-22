54007729faf4ee24c8118d16cc722be38c8f7d69
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-05-22 20:30:24 -0300

    Utilizando endpoint refresh token

--- system-back/src/main/java/com/starking/systemback/controllers/RefreshTokenController.java
+++ system-back/src/main/java/com/starking/systemback/controllers/RefreshTokenController.java
@@ -1,7 +1,17 @@
 package com.starking.systemback.controllers;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.starking.systemback.model.RefreshToken;
+import com.starking.systemback.model.request.RefreshTokenRequest;
+import com.starking.systemback.model.response.TokenResponse;
+import com.starking.systemback.services.JwtService;
+import com.starking.systemback.services.RefreshTokenService;
+
+import lombok.AllArgsConstructor;
 
 /**
  * @author pedroRhamon
@@ -9,6 +19,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/refresh")
+@AllArgsConstructor
 public class RefreshTokenController {
 
+	private final JwtService jwtService;
+	
+	private final RefreshTokenService refreshTokenService;
+	
+	@PostMapping("/refreshToken")
+	public TokenResponse refreshToken(@RequestBody RefreshTokenRequest refreshTokenRequestDTO){
+	    return refreshTokenService.findByToken(refreshTokenRequestDTO.getToken())
+	            .map(refreshTokenService::verifyExpiration)
+	            .map(RefreshToken::getUserInfo)
+	            .map(userInfo -> {
+	                String accessToken = jwtService.GenerateToken(userInfo.getUsername());
+	                return JwtResponseDTO.builder()
+	                        .accessToken(accessToken)
+	                        .token(refreshTokenRequestDTO.getToken()).build();
+	            }).orElseThrow(() ->new RuntimeException("Refresh Token is not in DB..!!"));
+	}
 }
