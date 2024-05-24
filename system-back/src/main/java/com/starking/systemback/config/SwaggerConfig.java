358510da03458bd0559114583300f1dc5fd4623b
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-05-24 20:15:38 -0300

    adicionando anotacao

--- system-back/src/main/java/com/starking/systemback/config/SwaggerConfig.java
+++ system-back/src/main/java/com/starking/systemback/config/SwaggerConfig.java
@@ -1,8 +1,11 @@
 package com.starking.systemback.config;
 
+import org.springframework.context.annotation.Configuration;
+
 /**
  * @author pedroRhamon
  */
+@Configuration
 public class SwaggerConfig {
 
 }
