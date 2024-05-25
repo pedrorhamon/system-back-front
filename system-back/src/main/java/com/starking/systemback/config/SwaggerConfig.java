133f01aa8caf206668b941ee524afe1faab7efe0
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-05-25 11:06:39 -0300

    implements no WebMvcConfig

--- system-back/src/main/java/com/starking/systemback/config/SwaggerConfig.java
+++ system-back/src/main/java/com/starking/systemback/config/SwaggerConfig.java
@@ -1,11 +1,17 @@
 package com.starking.systemback.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author pedroRhamon
  */
 @Configuration
-public class SwaggerConfig {
+@EnableSwagger2
+public class SwaggerConfig implements WebMvcConfigurer {
+	
+	
 
 }
