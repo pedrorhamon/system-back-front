0dd05ec132eb080f148c12be2fc487d670f4a21d
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-05-26 11:11:22 -0300

    informacao da api

--- system-back/src/main/java/com/starking/systemback/config/SwaggerConfig.java
+++ system-back/src/main/java/com/starking/systemback/config/SwaggerConfig.java
@@ -7,8 +7,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -41,6 +43,14 @@ public class SwaggerConfig implements WebMvcConfigurer {
 				.build().securityContexts(Arrays.asList(securityContext())).securitySchemes(Arrays.asList(apiKey()))
 				.apiInfo(apiInfo());
 		
+	}
+	
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("API")
+				.description("Api de sistemas nao está definido ainda")
+				.version("1.0")
+				.contact(contact()).build();
 	}
 
 
