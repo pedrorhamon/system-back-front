a57a02af9bd81c46a6009815cfb6b5be8e840927
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-05-26 11:12:34 -0300

    adicionando o contact

--- system-back/src/main/java/com/starking/systemback/config/SwaggerConfig.java
+++ system-back/src/main/java/com/starking/systemback/config/SwaggerConfig.java
@@ -11,6 +11,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -52,6 +53,13 @@ public class SwaggerConfig implements WebMvcConfigurer {
 				.version("1.0")
 				.contact(contact()).build();
 	}
+	
+	private Contact contact() {
+		return new Contact("Pedro Rhamon Sousa Ferreira", 
+				"https://github.com/pedrorhamon/pedrorhamon",
+				"pedrorhamon16@gmail.com");
+	}
+
 
 
 }
