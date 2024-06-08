d3083b1458494edb23afd17c76cdad411cb390c8
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-06-08 20:42:00 -0300

    limpando tela de login os componentes

--- money-front/src/app/login/login.component.ts
+++ money-front/src/app/login/login.component.ts
@@ -42,44 +42,4 @@ export class LoginComponent implements OnInit {
     }
   )
   }
-
-  onSubmit(): void {
-    if (this.form.valid) {
-      const usuario: Usuario = this.form.value;
-      if (this.isUpdating) {
-        this.usuarioService.atualizarUsuario(usuario)
-          .subscribe(
-            (response: any) => {
-              console.log('Usuário atualizado com sucesso:', response);
-            },
-            error => {
-              console.error('Erro ao atualizar usuário:', error);
-            }
-          );
-      } else {
-        this.usuarioService.salvarUsuario(usuario)
-          .subscribe(
-            (response: any) => {
-              console.log('Usuário salvo com sucesso:', response);
-            },
-            error => {
-              console.error('Erro ao salvar usuário:', error);
-            }
-          );
-      }
-    }
-  }
-
-  loadUserForEdit(userId: number): void {
-    this.usuarioService.getUsuarioById(userId).subscribe(
-      (usuario: Usuario) => {
-        this.currentUser = usuario;
-        this.form.patchValue(usuario);
-        this.isUpdating = true;
-      },
-      error => {
-        console.error('Erro ao carregar usuário:', error);
-      }
-    );
-  }
 }
